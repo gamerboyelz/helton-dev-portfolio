ac8f4434810802faf36032540d52e244c54f72a1
gamerboyelz <81883477+gamerboyelz@users.noreply.github.com>
2023-01-25 22:40:26 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+const NavBar = () => {
+  return (
+    <div className="container-NavBar">
+      <div className="container-NavBar-icons">
+        <LinkedInIcon/>
+        <GitHubIcon/>
+        <WhatsAppIcon/>
+
+      </div>
+      <div className="container-NavBar-Links">
+        <h1>
+          SUMMARY
+        </h1>
+        <h1>
+          WORK
+        </h1>
+        <h1>
+          SKILLS
+        </h1>
+        <h1>
+          PROJECT
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      Hello
     </div>
   );
 }
