1f96ece6735fc76f24503017bf07e0e427f3f1c2
gamerboyelz <81883477+gamerboyelz@users.noreply.github.com>
2023-10-31 02:11:25 -0500

    Created new components and added new styles. the hero section was modifies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import gitlab_icon from './assets/gitlab-icon.png';
+import heroImage from './assets/heroImage.png';
+
 import "./App.styles.scss"
 
 
@@ -33,10 +35,34 @@ const NavBar = () => {
   );
 };
 
+const HeroSection = ()=> {
+  return(
+    <div className="container-Hero-Section">
+      <div>
+        <h1>Heading</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptatem incidunt quaerat vero animi voluptatibus quis, distinctio perferendis et porro assumenda pariatur fugiat? Deleniti, totam? Labore sequi officiis aperiam ut.</p> <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum saepe repellendus voluptatibus quis totam. Cum dolore, dolor ?</p><br />
+        <button className='btn-HeroSection'>HIRE ME</button>
+      </div>
+      <div><img src={heroImage} id='heroSectionImage' alt="" /></div>
+
+    </div>
+  );
+}
+
+const HorizontalRule = ()=> {
+  return(
+    <div className='horizontalRuleContainer'><hr className='horizontalRule'/></div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar /> 
+      <HeroSection />
+      <HorizontalRule />
      
     </div>
   );
