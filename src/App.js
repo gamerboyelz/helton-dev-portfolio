269b022210af9d1117bae932f5baccb79c81adbb
gamerboyelz <81883477+gamerboyelz@users.noreply.github.com>
2023-11-03 00:22:26 -0500

    The Detail list component was created using and array of objects that were rendered to the UI using the array map method

--- src/App.js
+++ src/App.js
@@ -1,69 +1,112 @@
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import gitlab_icon from './assets/gitlab-icon.png';
-import heroImage from './assets/heroImage.png';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import gitlab_icon from "./assets/gitlab-icon.png";
+import heroImage from "./assets/heroImage.png";
 
-import "./App.styles.scss"
-
+import "./App.styles.scss";
 
 const NavBar = () => {
   return (
     <div className="container-NavBar">
       <div className="container-NavBar-icons">
-        <LinkedInIcon/>
-        <GitHubIcon/>
-        <WhatsAppIcon/>
-        <img className='gitLab' src={gitlab_icon} alt=""/>
+        <LinkedInIcon />
+        <GitHubIcon />
+        <WhatsAppIcon />
+        <img className="gitLab" src={gitlab_icon} alt="" />
       </div>
 
       <div className="container-NavBar-Links">
-        <div>
-          SUMMARY
-        </div>
-        <div>
-          WORK
-        </div>
-        <div>
-          SKILLS
-        </div>
-        <div>
-          PROJECT
-        </div>
+        <div>SUMMARY</div>
+        <div>WORK</div>
+        <div>SKILLS</div>
+        <div>PROJECT</div>
       </div>
     </div>
   );
 };
 
-const HeroSection = ()=> {
-  return(
+const HeroSection = () => {
+  return (
     <div className="container-Hero-Section">
       <div>
         <h1>Hi, I'm Helton</h1>
-        <p>A BSC. Information Communication and Technology Graduate with a Major in Software Engineer. Lets work together on your projects.</p> <br />
-        <p>Thid website was create with REACT.</p><br />
-        <button className='btn-HeroSection'>HIRE ME</button>
+        <p>
+          A BSC. Information Communication and Technology Graduate with a Major
+          in Software Engineer. Lets work together on your projects.
+        </p>{" "}
+        <br />
+        <p>Thid website was create with REACT.</p>
+        <br />
+        <button className="btn-HeroSection">HIRE ME</button>
       </div>
-      <div><img src={heroImage} id='heroSectionImage' alt="" /></div>
-
+      <div>
+        <img src={heroImage} id="heroSectionImage" alt="" />
+      </div>
     </div>
   );
-}
+};
 
-const HorizontalRule = ()=> {
-  return(
-    <div className='horizontalRuleContainer'><hr className='horizontalRule'/></div>
-  )
-}
+const HorizontalRule = () => {
+  return (
+    <div className="horizontalRuleContainer">
+      <hr className="horizontalRule" />
+    </div>
+  );
+};
+// here
+const MainDetailList = () => {
+  const detailListInfo = [
+    {
+      id: 1,
+      heading: "Summary",
+      informationA: "Something about  Summary",
+      informationB: "Something about  Summary"
+    },
+    {
+      id: 2,
+      heading: "Work",
+      informationA: "Something bout Work experience",
+      informationB: "Something bout Work experience"
+    },
+    {
+      id: 3,
+      heading: "Skills",
+      informationA: "Something bout Work experience",
+      informationB: "Something bout Work experience"
 
+    },
+    {
+      id: 4,
+      heading: "Projects",
+      informationA: "Something bout Work experience",
+      informationB: "Something bout Work experience"
+
+    },
+  ];
+
+  return (
+    <div>
+      {detailListInfo.map((detailListInfoObjects) => {
+        return (
+          <div>
+            <h1>{detailListInfoObjects.heading}</h1>
+            <p>{detailListInfoObjects.informationA}</p>
+            <p>{detailListInfoObjects.informationB}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <NavBar /> 
+      <NavBar />
       <HeroSection />
       <HorizontalRule />
-     
+      <MainDetailList />
     </div>
   );
 }
