1847ac6b2e76395cd924b8df58fbfdf5938b347e
gamerboyelz <81883477+gamerboyelz@users.noreply.github.com>
2024-05-21 23:43:44 -0500

    fix issues with mobile navigation menu, style mobile nav bar

--- src/components/hamburgerMenuComponent.jsx
+++ src/components/hamburgerMenuComponent.jsx
@@ -1,11 +1,28 @@
 import { HiOutlineMenu } from "react-icons/hi";
 import { IconContext } from "react-icons";
+import scrollToSection from "../utilities/smootheScrollUtility";
+import { useState } from "react";
 
 const HamBurgerMenu = ()=>{
 
+    const smootheScrollToSection = scrollToSection
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const toggleHamBurgerMenu = ()=>{
+        setOpenMenu(!openMenu)
+    }
+
+
     return(
         <IconContext.Provider value={{size: "2.5em"}}>
-        <div className="hamBurgerMenu"><HiOutlineMenu/></div>
+        <div className= "hamBurgerMenu" onClick={toggleHamBurgerMenu}><HiOutlineMenu/></div>
+        <div className= {`menu ${openMenu ? 'open' : ''}`}>
+        <div onClick={ ()=>{smootheScrollToSection('1') }}> SUMMARY </div>
+          <div onClick={ ()=>{smootheScrollToSection('2')} } > WORK </div>
+          <div onClick={ ()=>{smootheScrollToSection('3')}}> SKILLS </div>
+          <div onClick={ ()=>{smootheScrollToSection('4')} }>PROJECT</div>
+          <div>BLOG</div>
+        </div>
         </IconContext.Provider>
     )
 }
